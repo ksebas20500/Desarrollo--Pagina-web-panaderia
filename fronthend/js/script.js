87fd80343e2c06b849d9c37f8b1a6cc549766265
Ksebas20500 <kevinsebas5020@gmail.com>
2026-03-22 10:02:15 -0500

    Primer commit: Home, Header, Servicios y Productos

---
+++ fronthend/js/script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("El sitio web de La Fábrica está cargado correctamente.");
+
+    // Aquí agregaremos la interactividad más adelante:
+    // - Menú desplegable en teléfonos móviles
+    // - Lógica para mostrar los productos del catálogo
+});
